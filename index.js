2fd644bee647108a82f88602c7fcce28c2ddce56
chen3yym <40581631+chen3yym@users.noreply.github.com>
2023-01-21 02:40:30 -0600

    replace with components

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+/**
+Challenge: 
+
+- Move the `footer` into its own component called "Footer" 
+  and render that component inside the Page component.
+- Move the `h1` and `ol` together into another component
+  called "MainContent" and render inside Page as well.
+*/
+
 function Header() {
     return (
         <header>
@@ -11,10 +20,10 @@ function Header() {
     )
 }
 
-function Page() {
+
+function MainContent(){
     return (
-        <div>
-            <Header />
+        <>
             <h1>Reasons I'm excited to learn React</h1>
             <ol>
                 <li>It's a popular library, so I'll be 
@@ -22,9 +31,24 @@ function Page() {
                 <li>I'm more likely to get a job as a developer
                 if I know React</li>
             </ol>
+        </>
+    )
+}
+
+function Footer(){
+    return (
             <footer>
                 <small>© 2021 Ziroll development. All rights reserved.</small>
             </footer>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
     )
 }
