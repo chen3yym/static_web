e292f9a0e03ff91021ec9c660bf838b1da9f06a2
chen3yym <40581631+chen3yym@users.noreply.github.com>
2023-01-21 02:35:45 -0600

    build components using functions

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-function TemporaryName() {
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
+    )
+}
+
+function Page() {
     return (
         <div>
-            <img src="./react-logo.png" width="40px" />
-            <h1>Fun facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
-            </ul>
+            <Header />
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
+            <footer>
+                <small>© 2021 Ziroll development. All rights reserved.</small>
+            </footer>
         </div>
     )
 }
 
-ReactDOM.render(<TemporaryName />, document.getElementById("root"))
+ReactDOM.render(<Page />, document.getElementById("root"))
